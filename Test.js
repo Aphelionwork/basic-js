2e8a4d55d996e943b850094b21aedaa43e74efc3
Aphelionwork <aphelionwork@gmail.com>
2023-04-17 23:10:34 +0200

    solve 11 tasks

--- Test.js
+++ Test.js
@@ -1,41 +1,6 @@
 function getSeason(date) {
-  if (date === undefined) return 'Unable to determine the time of year!';
-  
-    //stackoverflow
-  if (date instanceof Date === false)
-  {
-    console.log("Invalid date!");
-    return "Invalid date!";
-  } 
-
-    let month = date.getMonth()+1;
-    let season = '';
-
-      switch (month) {
-      case 12:
-      case 1:
-      case 2:
-        season = 'winter';
-        break;
-      case 3:
-      case 4:
-      case 5:
-        season = 'spring';
-        break;
-      case 6:
-      case 7:
-      case 8:
-        season = 'summer';
-        break;
-      case 9:
-      case 10:
-      case 11:
-        season = 'autumn';
-        break;
-      }
-      return season;
+  console.log(date^0)
    
 }
 
-//getSeason(new Date(2025, 02, 22, 23, 45, 11, 500));
-getSeason("fefef");
+getSeason(65121);
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,26 +5,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    this.chain.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position === 'number' && (position^0) === position) {
+      if (position <1 || position >this.chain.length) {
+        this.chain.length = 0;
+        throw Error(`You can't remove incorrect link!`)
+      }
+      const index = position - 1;
+      this.chain.splice (index,1);
+      return this
+    }
+    this.chain.length = 0;
+    throw Error(`You can't remove incorrect link!`)
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const finish = this.chain.join('~~');
+    this.chain.length = 0;
+    return finish;
   }
+
+
 };
 
 module.exports = {
