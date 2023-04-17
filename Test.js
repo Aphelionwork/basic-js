3d91a97860a7c2e54293b21728440733d92ead48
Aphelionwork <aphelionwork@gmail.com>
2023-04-17 23:57:07 +0200

    solve 12 tasks

--- Test.js
+++ Test.js
@@ -1,6 +1,22 @@
-function getSeason(date) {
-  console.log(date^0)
-   
+function deleteDigit(n) {
+  const arrayOfDigits = Array.from(String(n), Number);
+  let i=0;
+  let j=1;
+  for (x=1; x<arrayOfDigits.length;x++) {
+    if (arrayOfDigits[i]===arrayOfDigits[j]){
+      i++;
+      j++;
+    } else {   
+        if (arrayOfDigits[j]>arrayOfDigits[i]) {
+        arrayOfDigits.splice(i, 1);
+        } else{
+        arrayOfDigits.splice(j, 1);
+        }
+        x=arrayOfDigits.length
+        console.log(Number(arrayOfDigits.join('')))
+        //return Number(arrayOfDigits.join(''));
+    }  
+  }
 }
 
-getSeason(65121);
+deleteDigit(152);
--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arrayOfDigits = Array.from(String(n), Number);
+  let i=0;
+  let j=1;
+  for (x=1; x<arrayOfDigits.length;x++) {
+    if (arrayOfDigits[i]===arrayOfDigits[j]){
+      i++;
+      j++;
+    } else {   
+        if (arrayOfDigits[j]>arrayOfDigits[i]) {
+        arrayOfDigits.splice(i, 1);
+        } else{
+        arrayOfDigits.splice(j, 1);
+        }
+        x=arrayOfDigits.length
+        //console.log(Number(arrayOfDigits.join('')))
+        return Number(arrayOfDigits.join(''));
+    }  
+  }
 }
 
 module.exports = {
