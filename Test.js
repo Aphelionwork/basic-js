fda9fcf558415d3c16cdc7d87bae56d401225204
Aphelionwork <aphelionwork@gmail.com>
2023-04-17 21:03:22 +0200

    solve 10 tasks

--- Test.js
+++ Test.js
@@ -1,46 +1,41 @@
-function getCommonCharacterCount(string1, string2) {
-  let newArray=[];
-  let array1 =  string1.split('');
-  let array2 =  string2.split('');
-  console.log(array1);
-  console.log(array2);
-  array1.forEach(function (element){
-  console.log(element,"элемент из первого массива" );
-        if (array2.includes(element)) {
-        newArray.push(element);
-        delete array2[array2.indexOf(element)];
-        //console.log(array2.indexOf(element), "индекс из второго массива");
-        console.log(newArray, "создаваеммый массив");
-        console.log(array2, "обновленный второй массив");
-        }  
-  });
- 
+function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  
+    //stackoverflow
+  if (date instanceof Date === false)
+  {
+    console.log("Invalid date!");
+    return "Invalid date!";
+  } 
 
-console.log(newArray);
-console.log(newArray.length);
+    let month = date.getMonth()+1;
+    let season = '';
+
+      switch (month) {
+      case 12:
+      case 1:
+      case 2:
+        season = 'winter';
+        break;
+      case 3:
+      case 4:
+      case 5:
+        season = 'spring';
+        break;
+      case 6:
+      case 7:
+      case 8:
+        season = 'summer';
+        break;
+      case 9:
+      case 10:
+      case 11:
+        season = 'autumn';
+        break;
+      }
+      return season;
+   
 }
 
-getCommonCharacterCount('abca', 'xyzbac');
-
-
- /*
-   let newArray = []; 
-  let i,j;
-  let array1 =  string1.split('');
-  let array2 =  string2.split('');
-  //console.log(array1);
-  //console.log(array2);
-      for (i=0; i<array1.length; i++) {
-        //console.log(array1[i], "первый массив");
-        for (j=0; j<array2.length;j++) {
-          //console.log(array2[j], "второй массив");
-          if (array2[j] === array1[i]){
-          newArray.push(array1[i]);
-          //console.log(newArray);
-          }
-          else { console.log('нет совпадения');
-          };
-        }; 
-      }
-  //console.log(Array.from(new Set(newArray)).length);
-  return Array.from(new Set(newArray)).length */
+//getSeason(new Date(2025, 02, 22, 23, 45, 11, 500));
+getSeason("fefef");
